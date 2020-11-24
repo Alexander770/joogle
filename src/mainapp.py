0b9d51c73b3dd0518ac01082bf215d80b4e45437
Alexander770 <alex.hazan@gmail.com>
2020-11-24 16:33:24 +0200

    asd

--- src/mainapp.py
+++ src/mainapp.py
@@ -16,8 +16,6 @@ logging.basicConfig(
     level = logging.INFO,
     format='[%(asctime)s] %(levelname)s:%(name)s:%(message)s',
     filename = 'C:\\Users\\Admin\\work\\main.log'
-    #
-    #
 )
 
 epoch = datetime(1970, 1, 1)
